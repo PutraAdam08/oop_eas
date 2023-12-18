baebfe7488925263eed07645c3c456555719f5aa
MoChikayo <127732169+MoChikayo@users.noreply.github.com>
2023-12-18 12:33:53 +0700

    Update Client.java

--- serverclient/src/serverclient/Client.java
+++ serverclient/src/serverclient/Client.java
@@ -1,18 +1,32 @@
 package serverclient;
-
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 8085);
+        if (args.length != 2) {
+            System.err.println("Usage: java Client");
+            System.exit(1);
+        }
 
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+            Socket socket = new Socket(hostName, portNumber);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            String serverMessage;
+            String userAnswer;
 
-            String serverMessage = in.readLine();
+
+            serverMessage = in.readLine();
             System.out.println("Server says: " + serverMessage);
 
             while (true) {
@@ -24,20 +38,18 @@ public class Client {
                 System.out.println("Server asks: " + mathQuestion);
 
                 System.out.print("Enter your answer: ");
-                int answer = Integer.parseInt(userInput.readLine()); // For simplicity, assume the answer is always an integer.
-                out.println(answer);
+                userAnswer = userInput.readLine();
+                out.println(userAnswer);
 
+                serverMessage = in.readLine();
+                System.out.println("Server says: " + serverMessage);
 
-                String serverResponse = in.readLine();
-                System.out.println("Server says: " + serverResponse);
-
-                if (serverResponse.toLowerCase().contains("play again")) {
+                if (serverMessage.toLowerCase().contains("play again")) {
 
                     System.out.print("Do you want to play again? (yes/no): ");
                     String playAgainResponse = userInput.readLine();
 
                     out.println(playAgainResponse.toLowerCase());
-                    
 
                     if ("no".equalsIgnoreCase(playAgainResponse)) {
                         break;
@@ -47,14 +59,12 @@ public class Client {
                     break;
                 }
             }
-
-            userInput.close();
-            in.close();
-            out.close();
-            socket.close();
-
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
         }
     }
 }
